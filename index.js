8fdb7248217ae7725bb783e3d946ff2a75fa4718
Sirasit Thiancharoenchai <ootcazy007@gmail.com>
2024-02-19 23:15:44 +0700

    add

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const plusHandler = (req, res) => {
 };
 
 app.get("/", (req, res) => res.send("Hello World!"))
-app.get("/getcode", (req, res) => res.send("Oot the most handsome person in the universe!!!"))
+app.get("/getcode", (req, res) => res.send("Oot the most handsome person~~~"))
 app.get("/plus/:num1/:num2",plusHandler)
 const server = app.listen(3001, () => console.log("Example app listening on port 3001!"))
 
