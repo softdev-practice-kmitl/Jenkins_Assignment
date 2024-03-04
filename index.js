2465f338693d17dfb52e6cc7eca0213147fd0228
Noppapor <por_por14968@hotmail.com>
2024-03-04 21:11:29 +0700

    newadd

--- index.js
+++ index.js
@@ -6,12 +6,26 @@ const plusHandler = (req, res) => {
     res.json(result);
 };
 
+const primeNumber = (req, res) => {
+    const num = parseInt(req.params.num);
+    let result = true;
+    for (let i = 2; i < num; i++) {
+      if (num % i === 0) {
+        result = false;
+        break;
+      }
+    }
+    res.json(result);
+  };
+
 app.get("/", (req, res) => res.send("Hello World!"))
-app.get("/getcode", (req, res) => res.send("Oot the most handsome person~~~ sud sud"))
+app.get("/getcode", (req, res) => res.send("Oot the most handsome person~~~"))
 app.get("/plus/:num1/:num2",plusHandler)
+app.get("/is_prime/:num",primeNumber)
 const server = app.listen(3001, () => console.log("Example app listening on port 3001!"))
 
 module.exports = {
     plusHandler,
+    primeNumber,
     server
 }
--- unit-test.test.js
+++ unit-test.test.js
@@ -1,15 +1,15 @@
-const { plusHandler,server } = require('./index'); // Import your Express route handler function
+const { plusHandler, server, primeNumber } = require("./index"); // Import your Express route handler function
 
-describe('Plus endpoint handler', () => {
-  test('returns the sum of two numbers passed in parameters', () => {
+describe("Plus endpoint handler", () => {
+  test("returns the sum of two numbers passed in parameters", () => {
     const req = {
       params: {
-        num1: '5',
-        num2: '10'
-      }
+        num1: "5",
+        num2: "10",
+      },
     };
     const res = {
-      json: jest.fn()
+      json: jest.fn(),
     };
 
     plusHandler(req, res);
@@ -17,23 +17,59 @@ describe('Plus endpoint handler', () => {
     expect(res.json).toHaveBeenCalledWith(15);
   });
 
-  test('returns NaN if parameters are not valid numbers', () => {
+  test("returns NaN if parameters are not valid numbers", () => {
     const req = {
       params: {
-        num1: 'abc',
-        num2: 'def'
-      }
+        num1: "abc",
+        num2: "def",
+      },
     };
     const res = {
-      json: jest.fn()
+      json: jest.fn(),
     };
 
     plusHandler(req, res);
 
     expect(res.json).toHaveBeenCalledWith(NaN);
   });
-});
 
-afterAll(done => {
+  test("true_when_x_is_17", () => {
+    const req = {
+      params: {
+        num: 17,
+      },
+    };
+    const res = {
+      json: jest.fn(),
+    };
+    primeNumber(req, res);
+    expect(res.json).toHaveBeenCalledWith(true);
+  }),
+    test("true_when_x_is_36", () => {
+      const req = {
+        params: {
+          num: 36,
+        },
+      };
+      const res = {
+        json: jest.fn(),
+      };
+      primeNumber(req, res);
+      expect(res.json).toHaveBeenCalledWith(false);
+    });
+  test("true_when_x_is_13219", () => {
+    const req = {
+      params: {
+        num: 13219,
+      },
+    };
+    const res = {
+      json: jest.fn(),
+    };
+    primeNumber(req, res);
+    expect(res.json).toHaveBeenCalledWith(true);
+  });
+});
+afterAll((done) => {
   server.close(done);
 });
